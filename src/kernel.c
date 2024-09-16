6edde3318ddb4d0ebe67eb3b99d26bfe632b62cf
Albano <albigoal@outlook.com>
2024-09-16 08:20:08 +0200

    kernel.c, linker.c

---
+++ src/kernel.c
@@ -0,0 +1,15 @@
+// kernel.c - Einfacher Kernel
+void kernel_main() {
+    const char *message = "Welcome,Ayanokoji";
+    char *video_memory = (char *) 0xb8000;  // VGA-Textspeicher bei 0xb8000
+    int i = 0;
+
+    while (message[i] != '\0') {
+        video_memory[i * 2] = message[i];   // Schreibe Zeichen
+        video_memory[i * 2 + 1] = 0x07;     // Setze Attribut (hellgrau auf schwarz)
+        i++;
+    }
+
+    while (1);  // Endlosschleife, damit der Kernel nicht endet
+}
+
